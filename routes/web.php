a5c0f1b5df4a857dd4ca764cbebb6f5f38303f18
Robben Lamtipando Sinaga <robbensinaga7@gmail.com>
2026-04-06 21:33:54 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/landing', function () {
     return view('landing_page');
 });
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
