7573e502852fb1bbead53b05ab8667de26af9e8b
Robben Lamtipando Sinaga <robbensinaga7@gmail.com>
2026-04-10 21:34:01 +0700

    tambah kode dan ganti kode

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Rental</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/landing', function () {
-    return view('landing_page');
+Route::get('/home', function () {
+    return view('home');
 });
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/app', function(){
+    return view('app');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/product', function () {
+    return view('product');
+});
