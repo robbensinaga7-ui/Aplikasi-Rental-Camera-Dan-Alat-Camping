35097094be794dc952dd16f3dcb465f6b8549514
Robben Lamtipando Sinaga <robbensinaga7@gmail.com>
2026-04-11 20:50:28 +0700

    tambah kode

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 Route::get('/home', function () {
     return view('home');
 });
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/app', function(){
     return view('app');
 });
 Route::get('/about', function () {
     return view('about');
 });
-Route::get('/product', function () {
-    return view('product');
-    
-use App\Http\Controllers\ProductController;
-
 Route::get('/product', [ProductController::class, 'index']);
-});
