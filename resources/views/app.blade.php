747a1a9c404e7aa3047cc74c3a6253dd322f21e1
john feddly nababan <feddly.nababan@gmail.com>
2026-04-12 21:34:33 +0700

    update kode

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite (['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mx-auto bg-blue-500">
+        <p class="text-lg font-bold bg">Hello, World!</p>
+    </div>
+</body>
+</html>
