e4c62c64a90de9f14973b24245a07b32a702f18e
Claudia M <claudyamanurung05@gmail.com>
2026-04-13 21:39:07 +0700

    tambah kode

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ Route::get('/app', function(){
 });
 Route::get('/register', function(){
     return view('register');
-})
+});
 Route::get('login', function(){
     return view('login');
-})
+});
 
 Route::get('/about', function () {
     return view('about');
